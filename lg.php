c3b77903f99faf61d9cc60a7481c65f3c8f0039c
bhayufebri <30520922+bhayufebri@users.noreply.github.com>
2019-01-23 11:46:14 +0700

    sdfffs
    ewe

--- lg.php
+++ lg.php
@@ -62,7 +62,7 @@
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In awdr</button>
         </div>
         <!-- /.col -->
       </div>
